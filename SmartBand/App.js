89ab21e14bfb47bfb390dedccf1d17ca2e320708
bangse94 <sjpark@bngt.kr>
2022-06-16 14:11:33 +0900

    add home/ble screens

--- SmartBand/App.js
+++ SmartBand/App.js
@@ -11,16 +11,23 @@ import React from 'react';
 import {
 	View,
 	Text,
-	TouchableOpacity,
 } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import HomeScreen from './src/HomeScreen';
+import BleScreen from './src/BleScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
 	return (
-		<>
-			<View>
-				
-			</View>
-		</>
+		<NavigationContainer>
+			<Stack.Navigator>
+				<Stack.Screen name="HomeScreen" component={HomeScreen} />
+				<Stack.Screen name="BleScreen" component={BleScreen} />
+			</Stack.Navigator>
+		</NavigationContainer>
 	)
 }
 
---
+++ SmartBand/src/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+	Text,
+	View,
+	TouchableOpacity,
+} from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+			<Text>HomeScreen</Text>
+		</View>
+	);
+}
